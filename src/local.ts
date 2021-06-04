4aa7b33e263784eef4ddd87d5379128220de23ef
TimHasert <timhasert@gmail.com>
2021-06-04 13:04:07 +0200

    prepared prod vs local mode

---
+++ src/local.ts
@@ -0,0 +1,28 @@
+/**
+ * @author TimH96
+ * local.ts
+ * 
+ * entrypoint script to run bot on local machine, supposed to be used with own account and channel only
+ */
+
+import AntiBurnerBot from './bot'
+import { AntiBurnerBotConfig } from './types/configs'
+
+let config: AntiBurnerBotConfig = {
+    identity: {
+        username: 'oddbod',
+        client_id: '5igx8b5i7o9al841l8e6w7a1nsfa23',
+        client_secret: 'goxl8rlv16m72l1hl1ncvx1l5gmf1s',
+        oauth_token: 'qopu26wx99okg3iehdigpb2ahmetky'
+    },
+	channels: [
+        {
+            name: 'oddbod',
+            min_age: 160000,
+            allowed_users: []
+        }
+    ],
+	run_locally: true
+}
+
+new AntiBurnerBot(config).run()
