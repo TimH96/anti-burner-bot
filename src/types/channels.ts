a9a38258ab38ea24a4df190af69878494934d3df
TimHasert <timhasert@gmail.com>
2021-06-04 13:02:54 +0200

    included allowed users array cache

--- src/types/channels.ts
+++ src/types/channels.ts
@@ -1,6 +1,7 @@
 /** IRC channel with bot metadata specific to that channel */
 export interface Channel{
-    api_id?: string;
-    name: string;
-    min_age: number;
+    api_id?: string
+    name: string
+    min_age: number
+    allowed_users: Array<string>
 }
